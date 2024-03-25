0e912d75f17f08bb5567b4ed86887eea7b9cf4ed
Tommy Rennolds <thomasrennolds@gmail.com>
2024-03-25 00:11:53 -0400

    close overlay by default

--- src/App.svelte
+++ src/App.svelte
@@ -320,7 +320,7 @@
 
   <main>
     <div class="container">
-    {#if hideOverlay}
+    {#if !hideOverlay}
     <div transition:slide class="gameover-overlay">
       <button class="exit-btn" on:click={toggleOverlay}>
         <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
