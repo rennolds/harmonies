fd6b50452e14075cb3a42e250a45ce1b349e6495
Tommy Rennolds <thomasrennolds@gmail.com>
2024-04-25 23:26:20 -0400

    new twitch logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -503,7 +503,9 @@
           {#if twitch}
           <button>
             <a href="{twitch}" target="_blank">
-              <svg fill="white" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"><path d="M2.149 0l-1.612 4.119v16.836h5.731v3.045h3.224l3.045-3.045h4.657l6.269-6.269v-14.686h-21.314zm19.164 13.612l-3.582 3.582h-5.731l-3.045 3.045v-3.045h-4.836v-15.045h17.194v11.463zm-3.582-7.343v6.262h-2.149v-6.262h2.149zm-5.731 0v6.262h-2.149v-6.262h2.149z" fill-rule="evenodd" clip-rule="evenodd"/></svg>
+              <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.852 4.716H11.568V9.852H9.852M14.568 4.716H16.284V9.852H14.568M4.284 0L0 4.284V19.716H5.136V24L9.432 19.716H12.852L20.568 12V0M18.852 11.148L15.432 14.568H12L9 17.568V14.568H5.136V1.716H18.852V11.148Z" fill="white"/>
+              </svg>
             </a>
           </button>
           {/if}
