ed9877358369bd4aded7157517e4acd8ae463bb0
Tommy Rennolds <thomasrennolds@gmail.com>
2026-01-03 22:51:28 -0500

    fixed

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,8 +1,9 @@
-export const load = ({ url }) => {
+export const load = ({ url, data }) => {
     // Query params are not part of pathname; main game is always `/`
     const isMainGame = url.pathname === '/';
     
     return {
+      ...data,  // Pass through server data (session, user, profile)
       isMainGame
     };
   };
